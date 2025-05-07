e73a7fe10f8aaac6a8579a39f74488e43302807b
atharavyadav <atharavyadav@gmail.com>
2025-05-07 21:56:34 +0530

    latest code

---
+++ e2e/pages/registerUser.js
@@ -0,0 +1,25 @@
+const browserManager = require("./browserResusableComponents");
+const logger = require("./logger");
+
+class RegisterUser {
+    constructor() {
+        // Define all XPath selectors in the constructor
+        this.firstNameInput = "//input[@name='firstName']";
+        this.lastNameInput = "//input[@name='lastName']";
+        this.phoneInput = "input[name='phone']";
+        this.emailInput = "//input[@id='userName']";
+    }
+
+    async entercontactInformation(firstname, lastname, phone, email) {
+        const page = browserManager.getPage(); // Get the page object
+        logger.log(`Entering contact information: ${firstname}, ${lastname}, ${phone}, ${email}`);
+        // Use the selectors defined in the constructor
+        await page.locator(this.firstNameInput).fill(firstname);
+        await page.locator(this.lastNameInput).fill(lastname);
+        await page.locator(this.phoneInput).fill(phone);
+        await page.locator(this.emailInput).fill(email);
+        logger.log("Contact information entered successfully.");
+    }
+}
+
+module.exports = new RegisterUser();
--- e2e/stepdefination/login_Steps.js
+++ e2e/stepdefination/login_Steps.js
@@ -1,7 +1,8 @@
 const { Given, When, Then, BeforeAll, AfterAll } = require("@cucumber/cucumber");
 const browserManager = require("../pages/browserResusableComponents");
 const logger = require("../pages/logger");
-const { expect } = require("assert");
+const RegisterUser = require("../pages/registerUser")
+
 
 BeforeAll(async function () {
     logger.initialize(); // Initialize the logger
@@ -20,13 +21,7 @@ Given('the user is on the login page', async function () {
 When(
     'the user enters contact information {string} {string} {string} {string}',
     async function (firstname, lastname, phone, email) {
-        const page = browserManager.getPage();
-        logger.log(`Entering contact information: ${firstname}, ${lastname}, ${phone}, ${email}`);
-        await page.locator("//input[@name='firstName']").fill(firstname);
-        await page.locator("//input[@name='lastName']").fill(lastname);
-        await page.locator('input[name="phone"]').fill(phone);
-        await page.locator("//input[@id='userName']").fill(email);
-        logger.log("Contact information entered successfully.");
+        RegisterUser.entercontactInformation(firstname,lastname,phone,email)
     }
 );
 
